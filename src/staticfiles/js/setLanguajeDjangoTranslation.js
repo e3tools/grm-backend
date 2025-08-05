bac1db51b1d64b161c89bf90478e5e92b7b41f21
Jorge García <jorgedgb@gmail.com>
2025-08-05 19:49:19 +0200

    chore: fix path to staticfiles in .gitignore and remove staticfiles/

--- src/staticfiles/js/setLanguajeDjangoTranslation.js
+++
@@ -1,8 +0,0 @@
-/*!
- * Submit the language change for Django Translation
- */
-
-$(document).on("click", ".language", function () {
-    document.language_form.language.value = $(this).data('language_code');
-    document.language_form.submit()
-});
