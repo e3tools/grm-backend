d1ddc0bf371df504bf4ab36f7d491d531a167ad2
Jorge García <jorgedgb@gmail.com>
2025-08-11 13:21:14 +0200

    feat: add endpoint to list issue types

--- src/issues/urls.py
+++ src/issues/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from issues.views import IssueCreateAPIView, IssueStatusListAPIView
+from issues.views import IssueCreateAPIView, IssueStatusListAPIView, IssueTypeListView
 
 app_name = "issues"
 urlpatterns = [
     path('push/', IssueCreateAPIView.as_view(), name='create-issue'),
     path('issue-statuses/', IssueStatusListAPIView.as_view(), name='list-issue-statuses'),
+    path('issue-types/', IssueTypeListView.as_view(), name='list-issue-types'),
 ]
