aab708b133d559936d48376158b618010d1169f1
AndreaSucreG <andresucre88@gmail.com>
2025-08-16 20:29:11 +0200

    fix test

--- src/issues/tests/unit_tests/test_issue.py
+++ src/issues/tests/unit_tests/test_issue.py
@@ -7,20 +7,17 @@ from gevent.testing import TestCase
 
 from grm.utils import reset_sequences
 from issues.factories import (
+    AdministrativeLevelFactory,
     AdministrativeRegionFactory,
     IssueCategoryFactory,
+    IssueDepartmentAdministrativeLevelFactory,
+    IssueDepartmentFactory,
     IssueFactory,
     IssueStatusFactory,
     IssueTypeFactory,
     UserFactory,
 )
-
-# Importa los modelos directamente, ya que se gestionarán manualmente
-from issues.models import (
-    AdministrativeLevel,
-    IssueDepartment,
-    IssueDepartmentAdministrativeLevel,
-)
+from issues.models import AdministrativeRegion
 
 
 @pytest.mark.django_db
@@ -31,15 +28,10 @@ class TestIssue(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """
-        Setup de la clase, se ejecuta una sola vez.
-        Crea las dependencias de datos que serán compartidas por todos los tests.
-        """
         super().setUpClass()
-        # Nivel administrativo
-        cls.administrative_level = AdministrativeLevel.objects.get_or_create(name="Country")[0]
+        AdministrativeRegion.objects.filter(parent__isnull=True).delete()
 
-        # Regiones administrativas
+        cls.administrative_level = AdministrativeLevelFactory(name="Country")
         cls.root_region = AdministrativeRegionFactory(
             name="Root Region",
             administrative_level=cls.administrative_level,
@@ -50,39 +42,26 @@ class TestIssue(TestCase):
             administrative_level=cls.administrative_level,
             parent=cls.root_region,
         )
-
-        # Departamentos y relaciones
-        cls.department = IssueDepartment.objects.get_or_create(name="Test Department")[0]
-        cls.department_admin_level = IssueDepartmentAdministrativeLevel.objects.get_or_create(
+        cls.department = IssueDepartmentFactory(name="Test Department")
+        cls.department_admin_level = IssueDepartmentAdministrativeLevelFactory(
             department=cls.department, administrative_level=cls.administrative_level
-        )[0]
-
-        # Categoría
+        )
         cls.category = IssueCategoryFactory(
             name="Test Category",
             assigned_department=cls.department_admin_level,
             assigned_appeal_department=cls.department_admin_level,
             assigned_escalation_department=cls.department_admin_level,
         )
-
-        # Tipo de problema, estado y usuarios
         cls.issue_type = IssueTypeFactory(name="Test Issue Type")
         cls.status = IssueStatusFactory(name="Test Status")
         cls.reporter = UserFactory()
         cls.assignee = UserFactory()
 
     def setUp(self):
-        """
-        Setup de la prueba, se ejecuta antes de cada test.
-        Ideal para limpiar la base de datos o resetear secuencias.
-        """
         reset_sequences()
         super().setUp()
 
     def test_str_representation(self):
-        """
-        Tests that the __str__ method returns the correct string format.
-        """
         issue = IssueFactory(
             status=self.status,
             category=self.category,
@@ -98,9 +77,6 @@ class TestIssue(TestCase):
         self.assertEqual(str(issue), expected_str)
 
     def test_resolution_days_with_resolution_date(self):
-        """
-        Tests the resolution_days method when the issue is resolved.
-        """
         issue = IssueFactory(
             intake_date=timezone.now() - timedelta(days=5),
             administrative_region=self.child_region,
@@ -115,9 +91,6 @@ class TestIssue(TestCase):
         self.assertEqual(issue.resolution_days(), 5)
 
     def test_resolution_days_without_resolution_date(self):
-        """
-        Tests the resolution_days method when the issue is not resolved.
-        """
         issue = IssueFactory(
             administrative_region=self.child_region,
             category=self.category,
@@ -129,9 +102,6 @@ class TestIssue(TestCase):
         self.assertIsNone(issue.resolution_days())
 
     def test_issue_is_created_with_default_contact_medium(self):
-        """
-        Tests that an Issue is created with the correct default contact_medium.
-        """
         issue = IssueFactory(
             contact_medium="channel-alert",
             administrative_region=self.child_region,
@@ -144,9 +114,6 @@ class TestIssue(TestCase):
         self.assertEqual(issue.contact_medium, "channel-alert")
 
     def test_issue_is_created_with_default_intake_date(self):
-        """
-        Tests that an Issue is created with a default intake_date set to now.
-        """
         issue = IssueFactory(
             administrative_region=self.child_region,
             category=self.category,
@@ -158,9 +125,6 @@ class TestIssue(TestCase):
         self.assertIsNotNone(issue.intake_date)
 
     def test_ongoing_issue_default_is_false(self):
-        """
-        Tests that the ongoing_issue field defaults to False.
-        """
         issue = IssueFactory(
             administrative_region=self.child_region,
             category=self.category,
@@ -172,9 +136,6 @@ class TestIssue(TestCase):
         self.assertFalse(issue.ongoing_issue)
 
     def test_automatic_tracking_code_generation(self):
-        """
-        Tests that the tracking_code is automatically populated upon saving.
-        """
         issue = IssueFactory(
             administrative_region=self.child_region,
             category=self.category,
@@ -186,9 +147,6 @@ class TestIssue(TestCase):
         self.assertIsNotNone(issue.tracking_code)
 
     def test_updated_date_updates_on_save(self):
-        """
-        Tests that the updated_date field updates on subsequent saves.
-        """
         issue = IssueFactory(
             administrative_region=self.child_region,
             category=self.category,
@@ -203,10 +161,6 @@ class TestIssue(TestCase):
         self.assertGreater(issue.updated_date, initial_updated_date)
 
     def test_contact_method_is_required_for_non_channel_alert_medium(self):
-        """
-        Tests that a ValidationError is raised if contact_medium is not 'channel-alert'
-        and contact_method is not provided.
-        """
         with self.assertRaises(ValidationError):
             IssueFactory(
                 contact_medium="facilitator",
@@ -220,10 +174,7 @@ class TestIssue(TestCase):
             )
 
     def test_contact_information_is_valid_for_email_method(self):
-        """
-        Tests that a ValidationError is raised if contact_method is 'email' but
-        contact_information is not a valid email address.
-        """
+
         with self.assertRaises(ValidationError):
             IssueFactory(
                 contact_method="email",
@@ -237,10 +188,6 @@ class TestIssue(TestCase):
             )
 
     def test_contact_information_is_valid_for_non_email_method(self):
-        """
-        Tests that a ValidationError is raised if contact_method is not 'email' but
-        contact_information is a valid email address.
-        """
         with self.assertRaises(ValidationError):
             IssueFactory(
                 contact_method="phone_number",
@@ -254,9 +201,6 @@ class TestIssue(TestCase):
             )
 
     def test_valid_issue_saves_correctly(self):
-        """
-        Tests that an issue with valid data saves without raising errors.
-        """
         issue = IssueFactory(
             contact_medium="facilitator",
             contact_method="phone_number",
@@ -275,10 +219,6 @@ class TestIssue(TestCase):
             self.fail("ValidationError was raised on a valid model instance.")
 
     def test_full_clean_is_called_on_save(self):
-        """
-        Tests that the _validate_contact_method_based_on_contact_medium method
-        is called during the save process.
-        """
         issue = IssueFactory.build(
             contact_medium="facilitator",
             contact_method=None,
