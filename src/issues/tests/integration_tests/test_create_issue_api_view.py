42a92903ad3b784a4fbca23341e8d455e97be72d
AndreaSucreG <andresucre88@gmail.com>
2025-08-20 23:34:12 +0200

    Add required fields in create issue api and more tests

--- src/issues/tests/integration_tests/test_create_issue_api_view.py
+++ src/issues/tests/integration_tests/test_create_issue_api_view.py
@@ -15,6 +15,7 @@ from issues.factories import (
     IssueDepartmentAdministrativeLevelFactory,
     IssueDepartmentFactory,
     IssueStatusFactory,
+    IssueSubTypeFactory,
     IssueTypeFactory,
     SubComponentFactory,
     UserFactory,
@@ -57,6 +58,8 @@ class TestIssueCreateAPIView(APITestCase):
             assigned_escalation_department=self.department_admin_level,
         )
         self.issue_type = IssueTypeFactory(name="Test Issue Type")
+        self.issue_sub_type = IssueSubTypeFactory(name="Test Issue SubType")
+
         self.initial_status = IssueStatusFactory()
         self.component = ComponentFactory()
         self.sub_component = SubComponentFactory()
@@ -66,37 +69,10 @@ class TestIssueCreateAPIView(APITestCase):
         self.citizen = CitizenFactory(age_group=self.age_group, group=self.group_one, group_2=self.group_two)
 
     def test_create_issue_with_valid_data(self):
-        """
-        Tests that an issue can be created with valid data.
-        """
         self.client.force_authenticate(user=self.reporter_user)
 
-        data = {
-            "title": "Test Issue",
-            "description": "This is a test issue.",
-            "status": self.initial_status.pk,
-            "category": self.issue_category.pk,
-            "issue_type": self.issue_type.pk,
-            "administrative_region": self.child_region.pk,
-            "reporter": self.reporter_user.pk,
-            "assignee": self.assignee_user.pk,
-            "citizen": {
-                "name": "Test Citizen",
-                "type": "organization_behalf_someone",
-                "age_group": self.age_group.pk,
-                "group": self.group_one.pk,
-                "group_2": self.group_two.pk,
-            },
-            "component": self.component.pk,
-            "sub_component": self.sub_component.pk,
-            "contact_medium": "channel-alert",
-            "contact_method": None,
-            "contact_information": "aa",
-            "ongoing_issue": True,
-            "tracking_code": "TRACK123",
-        }
+        data = self.__get_valid_data()
 
-        # Use reverse with the app namespace
         url = reverse("issues:create-issue")
         response = self.client.post(url, data=data, format="json")
 
@@ -106,24 +82,6 @@ class TestIssueCreateAPIView(APITestCase):
         self.assertEqual(created_issue.title, "Test Issue")
         self.assertEqual(created_issue.reporter, self.reporter_user)
         self.assertEqual(created_issue.administrative_region, self.child_region)
-
-    def test_create_issue_with_invalid_data(self):
-        """
-        Tests that an issue cannot be created with invalid data.
-        """
-        self.client.force_authenticate(user=self.reporter_user)
-
-        data = {
-            # Missing required fields like 'title'
-            "description": "This is a test issue.",
-        }
-
-        # Use reverse with the app namespace
-        url = reverse("issues:create-issue")
-        response = self.client.post(url, data=data, format="json")
-
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(Issue.objects.count(), 0)
 
     def test_create_issue_without_authentication(self):
         """
@@ -145,3 +103,58 @@ class TestIssueCreateAPIView(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(Issue.objects.count(), 0)
+
+    def test_create_issue_with_missing_required_field(self):
+        self.client.force_authenticate(user=self.reporter_user)
+
+        test_cases = [
+            {"expected_error_field": "title"},
+            {"expected_error_field": "description"},
+            {"expected_error_field": "issue_location"},
+            {"expected_error_field": "contact_medium"},
+            {"expected_error_field": "intake_date"},
+            {"expected_error_field": "issue_type"},
+            {"expected_error_field": "issue_sub_type"},
+            {"expected_error_field": "tracking_code"},
+        ]
+        url = reverse("issues:create-issue")
+
+        for case in test_cases:
+            data = self.__get_valid_data()
+            del data[case["expected_error_field"]]
+
+            with self.subTest(msg=f"Testing missing field: {case['expected_error_field']}"):
+                response = self.client.post(url, data=data, format="json")
+
+                self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+                self.assertIn(case["expected_error_field"], response.data['errors'])
+                self.assertEqual(Issue.objects.count(), 0)
+
+    def __get_valid_data(self):
+        return {
+            "title": "Test Issue",
+            "description": "This is a test issue.",
+            "status": self.initial_status.pk,
+            "category": self.issue_category.pk,
+            "issue_type": self.issue_type.pk,
+            "issue_sub_type": self.issue_sub_type.pk,
+            "issue_location": self.child_region.pk,
+            "intake_date": "2011-10-05T14:48:00.000Z",
+            "administrative_region": self.child_region.pk,
+            "reporter": self.reporter_user.pk,
+            "assignee": self.assignee_user.pk,
+            "citizen": {
+                "name": "Test Citizen",
+                "type": "organization_behalf_someone",
+                "age_group": self.age_group.pk,
+                "group": self.group_one.pk,
+                "group_2": self.group_two.pk,
+            },
+            "component": self.component.pk,
+            "sub_component": self.sub_component.pk,
+            "contact_medium": "channel-alert",
+            "contact_method": None,
+            "contact_information": "aa",
+            "ongoing_issue": True,
+            "tracking_code": "TRACK123",
+        }
