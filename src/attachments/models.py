b7a1ee8de6a56cdf4c268599511e1bc7c06a7d99
Jorge García <jorgedgb@gmail.com>
2025-09-10 18:15:03 +0200

    fix: missing changes on PR merge and fix for etl and tests

--- src/attachments/models.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import shortuuid as uuid
-
-
-def issue_attachment_upload_path(instance, filename):
-    filename, file_extension = os.path.splitext(filename)
-    filename = f"{uuid.uuid()}{file_extension}"
-    return f"attachments/{filename}"
